4d32857d9124e56cdb4478b7b41033914da0645e
Lordran <dantelopez305@outlook.com>
2023-11-06 16:33:04 -0600

    2opt , still needs to be checked

--- local_search.py
+++ local_search.py
@@ -11,4 +11,40 @@ matrix = np.array([
     [54, 61, 79, 56, 21, 70, 16, 0]
 ])
 
-init_tour = [1,2,3,4,5,6,7,8]
+init_tour = [0,1,2,3,4,5,6,7]
+
+def calculate_distance(tour, distance_matrix):
+    """Calculate the total distance of the tour."""
+    return sum(distance_matrix[tour[i-1]][tour[i]] for i in range(len(tour)))
+
+def two_opt(tour, distance_matrix, strategy='best'):
+    """Apply 2-OPT to improve the tour."""
+    while True:
+        best_distance = calculate_distance(tour, distance_matrix)
+        for i in range(len(tour) - 1):
+            for j in range(i+2, len(tour) + (i>0)):
+                if j == len(tour): j = 0  # wrap around to the beginning
+                new_tour = tour[:]
+                new_tour[i+1:j+1] = reversed(tour[i+1:j+1])  # this creates a new tour by reversing a section
+                new_distance = calculate_distance(new_tour, distance_matrix)
+                if new_distance < best_distance:  # if the new tour is shorter
+                    tour = new_tour  # make this the new current tour
+                    best_distance = new_distance
+                    if strategy == 'first':
+                        break  # if strategy is first found, break the loop after finding an improvement
+            if strategy == 'first' and new_distance < best_distance:
+                break
+        if best_distance == calculate_distance(tour, distance_matrix):
+            break  # if no improvement was found, end the algorithm
+    return tour
+
+print("Initial tour:", init_tour)
+print("Initial distance:", calculate_distance(init_tour, matrix))
+
+bf_tour = two_opt(init_tour, matrix, 'best')
+print("Best found tour:", bf_tour)
+print("Best found distance:", calculate_distance(bf_tour, matrix))
+
+ff_tour = two_opt(init_tour, matrix, 'first')
+print("First found tour:", ff_tour)
+print("First found distance:", calculate_distance(ff_tour, matrix))
